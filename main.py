aae75a3651ab92da06c2e048d3359085449920c0
Fernanda Matos <101283069+ferlimatos@users.noreply.github.com>
2026-03-03 20:55:36 -0300

    Adicionando funções no programa
    Hoje tivemos aula introdutória sobre funções. Estou adicionando as principais (que são citadas no README) e organizando o código do programa seguindo o conceito de DRY.

--- main.py
+++ main.py
@@ -11,6 +11,42 @@ dados_do_app = {
     "historico": [] # Uma lista que vai guardando cada transação
 }
 
+def percorrer_mês():
+    # Mostra os meses para o usuário
+    for i, nome_mes in enumerate(dados_do_app["meses"], start=1):
+          print(f"{i} - {nome_mes}")
+    
+def percorrer_despesas():
+    # Mostra os tipos de despesas para o usuário
+    for i, tipo_despesa in enumerate(dados_do_app["despesas"], start=1):
+          print(f"\n{i} - {tipo_despesa}")
+
+def cadastrar_receita():
+   # Adicionar valores ao dicionário do mês correspondente.
+   registro_receita = {
+        "mes": mes_escolhido, 
+        "valor": receita_atual
+        }
+   dados_do_app["historico"].append(registro_receita)
+
+def calcular_saldo():
+   # Realiza a subtração entre receitas e despesas, informando se o saldo está positivo ou negativo.
+  registro_gasto = {
+        "mes": mes_escolhido, 
+        "valor": gasto_atual, 
+        "tipo_despesa": despesa_escolhida
+  }
+  
+            
+  
+  dados_do_app["historico"].append(registro_gasto)
+
+"""def mostrar_relatorio():
+   # Identifica o mês de maior lucro e o mês de maior endividamento, sugerindo o valor necessário para a quitação.
+
+def imposto_gasto():
+   # Analisa qual categoria de despesa consumiu a maior parte do orçamento no período."""
+
 # Loop para o menu principal do programa
 while True:
   print('''Seja bem-vindo ao Oráculo Financeiro 2.0!
@@ -23,67 +59,52 @@ while True:
 
   match menu_escolha:
     case 1:
-      # Como mostrar as opções de meses
-      for i, nome_mes in enumerate(dados_do_app["meses"]):
-          print(f"{i} - {nome_mes}")
-
-      opcao_mes = int(input("Escolha o número do mês: "))
-      mes_escolhido = (dados_do_app["meses"])[opcao_mes]
+      percorrer_mês()
+      opcao_mes = int(input("\nEscolha o número do mês: "))
+      indice = opcao_mes - 1 
+      mes_escolhido = (dados_do_app["meses"])[indice]
+      print(f"Você escolheu: {mes_escolhido}")
 
       # Inserir valor da receita
       receita_atual = float(input('Digite um valor de sua receita: '))
       print(f'Você adicinou: R${receita_atual}')
+      dados_do_app.setdefault("valor", {receita_atual})
       print(f'Receita de R${receita_atual} adicionada no mês {mes_escolhido} com sucesso!')
 
-      # Salvando histórico
-      registro_receita = {
-        "mes": mes_escolhido, 
-        "valor": receita_atual
-        }
-      dados_do_app["historico"].append(registro_receita)
-
     case 2:
-      # Como mostrar as opções de meses
-      for i, nome_mes in enumerate(dados_do_app["meses"]):
-          print(f"{i} - {nome_mes}")
-
+      percorrer_mês()
       opcao_mes = int(input("Escolha o número do mês: "))
       mes_escolhido = (dados_do_app["meses"])[opcao_mes]
 
-      # Inserir valor da receita
+      # Inserir valor do gasto
       gasto_atual = float(input('Digite um valor de seu gasto: '))
       print(f'Você adicinou: R${gasto_atual}')
       print(f'Receita de R${gasto_atual} adicionada no mês {mes_escolhido} com sucesso!')
 
       # Mostrar as opções de despesas
-      for i, tipo_despesa in enumerate(dados_do_app["despesas"]):
-          print(f"{i} - {tipo_despesa}")
+      percorrer_despesas()
       opcao_despesa = int(input("Escolha o tipo de despesa: "))
       despesa_escolhida = dados_do_app["despesas"][opcao_despesa]
 
-      # Salvando histórico
-      registro_gasto = {
-        "mes": mes_escolhido, 
-        "valor": gasto_atual, 
-        "tipo_despesa": despesa_escolhida
-        }
-      dados_do_app["historico"].append(registro_gasto)
-
     case 3: 
-      mes_solicitado = input('Informe u ')
-      tipo_transacao = input('Você deseja ver todas as receitas ou gastos? ')
+      percorrer_mês()
+      opcao_mes = int(input("Escolha o número do mês: "))
+      mes_escolhido = (dados_do_app["meses"])[opcao_mes]
 
-      if tipo_transacao == 'receitas':
-        print() # Mostrar todas as receitas registradas do mês até o momento.
-      elif tipo_transacao == 'gastos':
-        print() # Mostrar todos os gastos registrados do mês até o momento.
-      elif tipo_transacao != "meses":
-        print() # Mostrar mensagem que está incorreto. 
+      tipo_transacao = input('Você deseja ver todas as receitas ou gastos? (Receitas/Gastos): ')
+
+      if tipo_transacao > 0:
+        for i, receita_mes_selecionado in dados_do_app['historico']:
+        # Mostrar todas as receitas registradas do mês até o momento.
+          print(f"Registro de todas as receitas do mês de {mes_escolhido}:")
+          print(f"{i}")
       else:
-        print() # Mostrar que ainda não foi adicionado nada, portanto não tem nada para ser mostrado.
-    case _:
+        print("Não há nenhuma transação registrada no histórico!")
+    case 4:
       print('Programa encerrado!')
       break
+    case _:
+        print('Comando inválido! Tente novamente.')
 
 # Para o case 3, você terá que percorrer o seu historico.
 # Melhorar o nome de dados e variáveis, organizar melhor o dicinário.
